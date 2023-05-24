232f4aa493679e48e09625721a37d27220be9e1a
adeola2020-git <aadefolaju@gmail.com>
2023-05-24 10:25:37 +0100

    my first commit

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+# variable "instance_ami" {
+#   default = "ami-08b064b1296caf3b2" 
+# }
+
+# variable "vpc_id" {
+#   default = ""
+# }
+
+variable "key_name" {
+  default = "cba-web-KP"
+}
